da05e7c9655ba4d7cd25609f07f364d7b070a9fe
Natalia Silva Bezerra <natalia.silva.bezerra@g.globo>
2026-02-17 23:27:37 -0300

    Integrate notification preferences into useUser hook for enhanced user experience
    - Added usePreferences hook to manage user notification settings for newsletters, offers, order status, and password changes.
    - Updated login function to set default notification preferences upon user login.
    - Improved user data management by consolidating notification settings within the useUser hook.

--- src/hooks/use-user.ts
+++ src/hooks/use-user.ts
@@ -1,11 +1,19 @@
 import { useMMKVString } from "react-native-mmkv";
 import { storage } from "@/lib/mmkvStorage";
+import { usePreferences } from "./use-preferences";
 
 export function useUser() {
   const [name, setName] = useMMKVString("userName", storage);
   const [image, setImage] = useMMKVString("userImage", storage);
   const [email, setEmail] = useMMKVString("userEmail", storage);
   const [phone, setPhone] = useMMKVString("userPhone", storage);
+
+  const {
+    setNotifyNewsletter,
+    setNotifyOffers,
+    setNotifyOrderStatus,
+    setNotifyPasswordChange,
+  } = usePreferences();
 
   function logout() {
     storage.clearAll();
@@ -14,6 +22,11 @@ export function useUser() {
   function login({ name, email }: { name: string; email: string }) {
     setName(name);
     setEmail(email);
+
+    setNotifyNewsletter("true");
+    setNotifyOffers("true");
+    setNotifyOrderStatus("true");
+    setNotifyPasswordChange("true");
   }
 
   return {
